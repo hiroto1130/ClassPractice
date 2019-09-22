ce6799fb36707c095c49d5d30f70580bbcee6c96
mono1140 <11potato30@gmail.com>
2019-09-22 17:38:25 +0900

    systemclassの作成(#2)

---
+++ CalssTest/ClassFunction/System.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <random>
+#include <iostream>
+#include <cstdlib>
+
+#include"System.h"
+
+System::System()
+{
+
+}
+
+System::~System()
+{
+
+}
+
+void System::CleanRog()
+{
+	std::system("cls");
+}
---
+++ CalssTest/ClassFunction/System.h
@@ -0,0 +1,15 @@
+﻿#ifndef SYSTEM_H
+#define SYSTEM_H
+
+class System
+{
+private:
+
+public:
+	System();
+	~System();
+	// ログ削除
+	void CleanRog();
+};
+
+#endif
